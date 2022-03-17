a77997c6d301485b95dff89e6613f58ccba5ec98
Sri <tommy24b@gmail.com>
2022-03-16 23:50:27 -0700

    commit

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,7 +1,23 @@
 <svelte:head>
-	<title>About</title>
+  <title>About</title>
 </svelte:head>
 
-<h1>About this site</h1>
+<h1>Founder</h1>
+<h1>Charlie Rugg</h1>
 
-<p>This is the 'about' page. There's not much here.</p>
+<p>
+  Born and raised in Boston, Charlie found success in sports at a young age.
+  Soccer became his focus in high school, where he earned All-American accolades
+  as well as being named the Gatorade Massachusetts boy’s soccer player of the
+  year for 2008-09. He earned a scholarship to Boston College where he played
+  forward for four years and Co-Captained the team in his senior season. In
+  2013, he was the 19th overall pick in the MLS draft, chosen by the LA Galaxy
+  where he began his professional soccer career. His three years with the club
+  included a loan period with Indy Eleven in the NASL, and an MLS Cup
+  Championship with LA, in 2014. Following his time in LA he spent eighteen
+  months playing with two clubs in Germany before returning to California and
+  beginning his coaching career. He began coaching at Central Coast United SC,
+  and joined the Boston College Men's staff as director of operations in 2020.
+  Charlie has extensive private coaching experience, and has found his passion
+  for coaching, on the mentoring side of the game.
+</p>
