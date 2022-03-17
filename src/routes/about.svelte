175c2f592edb72611ca541b76453571be01943cc
Sri <tommy24b@gmail.com>
2022-03-17 00:14:53 -0700

    commit

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,9 +1,17 @@
+<script>
+  import charlie from 'images/charlie.jpg'
+</script>
+
 <svelte:head>
   <title>About</title>
 </svelte:head>
 
 <h1>Founder</h1>
 <h1>Charlie Rugg</h1>
+<figure>
+  <img alt=" Charlie Rugg" src={charlie} />
+
+</figure>
 
 <p>
   Born and raised in Boston, Charlie found success in sports at a young age.
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -23,7 +23,7 @@
 
   img {
     width: 100%;
-    max-width: 400px;
+    max-width: 900px;
     margin: 0 0 1em 0;
   }
 
